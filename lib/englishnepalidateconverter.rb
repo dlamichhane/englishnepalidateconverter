ee32feb81d81d924965efd6c1da1e44fddc3f965
Deepak Lamichhane <dlamichhane@hotmail.com>
2014-08-20 15:04:39 +0300

    minor fixes

--- lib/englishnepalidateconverter.rb
+++ lib/englishnepalidateconverter.rb
@@ -7,5 +7,5 @@ require "englishnepalidateconverter/bs_date"
 require "englishnepalidateconverter/date_conversion"
 
 # date = EnglishNepaliDateConverter::DateConversion.new
-# date.nep_to_eng(00, 02, 2041)
+# date.nep_to_eng(01, 01, 2033)
 # date.eng_to_nep(42, 12, 2001)
